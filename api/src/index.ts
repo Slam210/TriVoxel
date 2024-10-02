7a341ec5deb24a4826f130d12fa85e3f080ce4f6
Steven Lam <stevenl.inbox@gmail.com>
2024-10-01 18:14:14 -0700

    Finishing base set up

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,9 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
+app.use("/api/auth", authRoutes);
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });

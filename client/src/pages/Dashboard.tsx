909e7f3c41e00ef0e145af823c0bc222c4fc3aa7
Steven Lam <stevenl.inbox@gmail.com>
2024-10-03 18:57:37 -0700

    Initial Sidebar

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,3 +1,28 @@
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+
+export default function Dashboard(): JSX.Element {
+  const location = useLocation();
+  const [tab, setTab] = useState<string | null>("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromURL = urlParams.get("tab");
+    if (tabFromURL) {
+      setTab(tabFromURL);
+    }
+  }, [location.search]);
+
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* Sidebar */}
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      {/* Right Side */}
+      <div>{tab === "profile" && <DashProfile />}</div>
+    </div>
+  );
 }
